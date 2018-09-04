97d036fd2bf296675a0a437688f84ea0c869af7f
Shlomi Fish <shlomif@shlomifish.org>
2018-09-04 08:48:32 +0300

    Add a .travis.yml file for Travis-CI.
    Based on https://github.com/shlomif/perl6-CI-Gen and
    https://github.com/shlomif/ci-gen-framework .
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

---
+++ .gen-ci.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# .gen-ci.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e
+set -x
+ci-generate --theme 'XML-Grammar-Fiction' --param 'screenplay_subdir=Muppets-Show-TNI/screenplay/'
